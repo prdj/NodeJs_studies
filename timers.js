1f2cd4599065aac466d7292a3d10fef99865ef1f
Pedro Dantas <pedrojr_dantas@hotmail.com>
2021-08-15 21:44:58 +0200

    setInterval

--- timers.js
+++ timers.js
@@ -1,6 +1,21 @@
-const waitTime = 3000;
-console.log(`setting a ${waitTime/1000} second delay`);
+const { setInterval } = require("timers");
 
-const timerFinished = () => console.log("done");
+const waitTime = 3000;
+const waitInterval = 500;
+let currenTime = 0;
+
+const incTime = () => {
+    currenTime += waitInterval;
+    console.log(`waiting ${currenTime / 1000} seconds`)
+};
+
+console.log(`setting a ${waitTime / 1000} second delay`);
+
+const timerFinished = () => {
+    clearInterval(interval);
+    console.log("done");
+};
+
+const interval = setInterval(incTime, waitInterval)
 
 setTimeout(timerFinished,waitTime);
