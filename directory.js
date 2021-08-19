f9bdb0ab65162112c2ae5f4a858a0e44d553d128
Pedro Dantas <pedrojr_dantas@hotmail.com>
2021-08-19 08:48:14 +0200

    directoryAlreadyThere

--- directory.js
+++ directory.js
@@ -1,9 +1,14 @@
 const fs = require("fs");
 
-fs.mkdir("storage-files", err => {
-    if (err) {
-        throw err;
-    }
+if ( fs.existsSync("storage-files")) {
+    console.log("Already there");
+} else {
+    fs.mkdir("storage-files", err => {
+        if (err) {
+            throw err;
+        }
+    
+        console.log("directory created");
+    });
+}
 
-    console.log("directory created")
-});
